26d1271c7f73aa98d96213293a21139002d68334
Nomynameisjames <adavaonimisi@gmail.com>
2023-03-31 02:01:20 +0100

    using variable annotation to return a float value

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+
 """This module contains a function that adds two floats"""
+
 def add(a: float, b: float) -> float:
     """
-        This function adds two floats and returns the result which is a
-        floating point number
+    This function adds two floats and returns the result which is a
+    floating point number
     """
+
     return a + b
