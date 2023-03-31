a301e2a45f486d905f05daac4aac5a5224d04f30
Nomynameisjames <adavaonimisi@gmail.com>
2023-03-31 02:38:31 +0100

    using the List annonated type

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+   declaring a list annonated type and summing the list
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+       summing the list
+    """
+    return sum(input_list)
