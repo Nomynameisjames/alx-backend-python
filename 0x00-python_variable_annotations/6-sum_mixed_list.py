6feab45c518196daf5c67a49fa04e806341118e4
Nomynameisjames <adavaonimisi@gmail.com>
2023-03-31 03:00:16 +0100

    using the List and Union method from the typing module

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from typing import List, Union
+
+"""
+    the module contains a function that sums a list of floats and ints
+    annotated values types and returns the sum as a float
+"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+        sums a list of floats and ints by using the List and Union
+        method from the typing module
+        annotated values types and returns the sum as a float
+    """
+    return sum(mxd_lst)
