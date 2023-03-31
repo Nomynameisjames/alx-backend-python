25e00e9b29618c855c89bb910f098a8319893f2b
Nomynameisjames <adavaonimisi@gmail.com>
2023-03-31 02:17:02 +0100

    using variable annotation to return a float value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+"""
+This module contains a function that returns the floor value passed to it
+"""
+
+
+def floor(n: float) -> float:
+    """
+    This function returns the floor value passed to it
+    """
+
+    return math.floor(n)
