d0bc6c089b12652a09d4ca55218da776b674fd70
Nomynameisjames <adavaonimisi@gmail.com>
2023-03-31 03:28:04 +0100

    using the List and Union method from the typing module

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 from typing import Union, Tuple
+
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Returns a tuple containing a string and the square of an integer or float.
+    Returns a tuple containing a string and the square of an
+    integer or float.
 
     Args:
         k (str): A string.
         v (Union[int, float]): An integer or float.
 
     Returns:
-        Tuple[str, float]: A tuple with string k and the square of the int/float v.
+        Tuple[str, float]: A tuple with string k and the square
+        of the int/float v.
     """
     return (k, float(v ** 2))
-
