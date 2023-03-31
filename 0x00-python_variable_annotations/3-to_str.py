1eb50d4e301ee02cc427734553516dd7e86bd23e
Nomynameisjames <adavaonimisi@gmail.com>
2023-03-31 02:29:44 +0100

    returns the string value of a float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function that returns the string value of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function returns the string value of a float
+    """
+
+    return str(n)
