596f114571267fceca10a749b7b8de386b760c67
Nomynameisjames <adavaonimisi@gmail.com>
2023-03-31 02:49:34 +0100

    fixed bug

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,12 +3,14 @@ import math
 
 """
 This module contains a function that returns the floor value passed to it
+by calling the Math module floor method
 """
 
 
 def floor(n: float) -> int:
     """
     This function returns the floor value passed to it
+    by calling the Math module floor method
     """
 
     return math.floor(n)
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 from typing import List
 """
-   declaring a list annonated type and summing the list
+   declaring a list annonated type and summing the list using
+   the sun_list function
 """
 
 
 def sum_list(input_list: List[float]) -> float:
     """
-       summing the list
+       summing the list of float values passed to the list
+       and returning the sum as a float value
     """
     return sum(input_list)
