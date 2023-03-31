d698875c63200212ff29820928c1307f770b4cfe
Nomynameisjames <adavaonimisi@gmail.com>
2023-03-31 02:10:38 +0100

    using variable annotation to return a str value

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function that adds two floats
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        This function add two annotated string  typed value and
+        returns the result which is  concatenated string
+    """
+
+    return str1 + str2
