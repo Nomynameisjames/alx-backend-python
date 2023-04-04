8751c4e8b3ad972e19538aaf3ba8b638dd5ad52a
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-04 01:39:13 +0100

    using async and await

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+''' import asyncio module to use async/await syntax
+    import random module to use random function
+'''
+import asyncio
+import random
+
+"""
+    function declares an async function named wait_random that waits
+    for a random delay between 0 and max_delay (included and float value)
+    seconds and eventually returns it.
+"""
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    waitTime = random.uniform(0, max_delay)
+    await asyncio.sleep(waitTime)
+    return waitTime
