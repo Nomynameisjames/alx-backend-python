b7c6e6fe98e1e203218e6f06dc28b530123c099e
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-05 00:32:37 +0100

    Yielding random numbers using a generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -14,7 +14,11 @@ import random
 
 
 async def async_generator():
-    ''' async_generator function '''
+    '''
+        async_generator function
+        coroutine that will loop 10 times,
+        each time asynchronously wait 1 second,
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
