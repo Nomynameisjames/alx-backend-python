a1f2e5ee6017dafd35cb015f2dad7f7d2b9d76e4
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-05 01:01:02 +0100

    Yielding random numbers using a async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
-''' import the asyncio module to use async/await
-    import the random module to generate random numbers
-    import the typing module to use AsyncGenerator type annotations
-'''
-import asyncio
+"""
+    import random to generate random numbers
+    import asyncio to create async functions
+    import typing to specify the type of the function
+"""
 import random
+import asyncio
 from typing import AsyncGenerator
 
-
-'''
-    a coroutine called async_generator that takes no arguments.
-    The coroutine will loop 10 times, each time asynchronously wait 1 second,
-    then yield a random number between 0 and 10.
-'''
+"""
+    async_generator function that takes no arguments
+    and returns a random number between 0 and 10
+"""
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    '''
-        async_generator function
-        coroutine that will loop 10 times,
-        each time asynchronously wait 1 second.
-    '''
-    for i in range(10):
+    """
+    The function yields a random number
+    """
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
